d83fd62384a5457b14ecaddaba5188746e3f1bfa
jpdoohan <johnpauldoohan@gmail.com>
2024-11-11 12:10:57 +0000

    Add files via upload
    First draft of a minimum viable product --
    To fix - Time calculations; incoporate switch statements for months with varying days, and see rocket in COM558 for similar time calcs.
    Going forwards; abstraction, encapsulation, inheritance,  polymorphism.

---
+++ FoodTrackerPrototype.java
@@ -0,0 +1,32 @@
+package GroupProject;
+
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class FoodTrackerPrototype {
+    public static void main(String[] args) { //main
+        Scanner scanner = new Scanner(System.in); //scanner object
+
+        //variables
+        String food = "food";
+        int date;
+        int daysLeft;
+
+        //user prompts
+        System.out.println("Enter food");
+        food = scanner.nextLine();
+
+        System.out.println("Enter date");
+        date = scanner.nextInt();
+
+        //days left calculation - basic
+        daysLeft = date - Calendar.getInstance().get(Calendar.DATE);
+
+        //print current day
+        System.out.println(Calendar.getInstance().get(Calendar.DATE));
+
+        //feedback
+        System.out.println("You have " + daysLeft + " days left to eat " + food);
+
+    }
+}
