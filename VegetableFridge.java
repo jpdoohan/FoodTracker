30e358b40245978e2ba907b2dcc44da1fa1b636f
jpdoohan <johnpauldoohan@gmail.com>
2024-11-25 11:40:36 +0000

    Add files via upload

---
+++ VegetableFridge.java
@@ -0,0 +1,25 @@
+package VegetableFridge;
+
+/**
+ * Name:Jp Doohan
+ * Date:11.16.24
+ * Purpose: Class to test methods for fridge - listed after class declaration
+ */
+
+public class VegetableFridge {
+    /**
+     * Update note added -11.17.24
+     * Method list :
+     * 0. homePage() -- display homepage-- see testMenu
+     *      1) print fridge
+     *          -getFridge() contents by reading a .txt
+     *          -create an array using the values of the .txt
+     *          -date subtraction of BB to current date dateSubtraction()
+     *          -printFridge() - print the array
+     *      2). updateFridge() --add a veg to the fridge and its best before date -- user input updates array
+     *          -testfridge class by Lucy
+     *          
+     *
+     */
+
+}
